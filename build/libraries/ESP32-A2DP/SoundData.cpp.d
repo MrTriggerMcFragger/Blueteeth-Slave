f0368853aaffa94c656559a9ec8bc02f51479d95
MrTriggerMcFragger <78004809+MrTriggerMcFragger@users.noreply.github.com>
2023-11-28 15:53:02 -0500

    Removed build folder from version control

--- build/libraries/ESP32-A2DP/SoundData.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\ztzac\Documents\GitHub\Blueteeth-Slave\build\libraries\ESP32-A2DP\SoundData.cpp.o: \
- C:\Users\ztzac\OneDrive\Documents\Arduino\libraries\ESP32-A2DP\src\SoundData.cpp \
- C:\Users\ztzac\OneDrive\Documents\Arduino\libraries\ESP32-A2DP\src\SoundData.h
